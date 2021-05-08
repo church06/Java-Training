de5afeb2d99306c83d82ae4d89515a9bcd307b07
SDesolator <protoss06@outlook.com>
2021-05-08 22:04:06 +0800

    test Fibonacci

---
+++ UoB/Semester_2/Data_Structrue/Training/Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        System.out.println(fibonacci_func(45));
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+
+    }
+
+    public static double fibonacci_func(int n) {
+        return (1 / Math.pow(5, 0.5)) * (Math.pow((1 + Math.pow(5, 0.5)) / 2, n) - Math.pow((1 - Math.pow(5, 0.5)) / 2, n));
+
+    }
+}
